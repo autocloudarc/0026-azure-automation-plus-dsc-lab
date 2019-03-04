1f42ba43f057c07986b66202a43ecb21fcbe70a2
autocloudarc <prestopa@microsoft.com>
2019-03-04 09:41:54 -0500

    Add web configuration.

---
+++ data/dsc/configurations/web/webAzrConfigReset.ps1
@@ -0,0 +1,38 @@
+Configuration WebAzrConfigReset {
+ Import-DscResource -ModuleName PSDesiredStateConfiguration
+ $features = @(
+  @{Name = "Web-Server"; Ensure = "Absent"},
+  @{Name = "Web-WebServer"; Ensure = "Absent"},
+  @{Name = "Web-Common-http"; Ensure = "Absent"},
+  @{Name = "Web-Default-Doc"; Ensure = "Absent"},
+  @{Name = "Web-Dir-Browsing"; Ensure = "Absent"},
+  @{Name = "Web-Http-Errors"; Ensure = "Absent"},
+  @{Name = "Web-Static-Content"; Ensure = "Absent"},
+  @{Name = "Web-Health"; Ensure = "Absent"},
+  @{Name = "Web-Http-Logging"; Ensure = "Absent"},
+  @{Name = "Web-Performance"; Ensure = "Absent"},
+  @{Name = "Web-Stat-Compression"; Ensure = "Absent"},
+  @{Name = "Web-Dyn-Compression"; Ensure = "Absent"},
+  @{Name = "Web-Security"; Ensure = "Absent"},
+  @{Name = "Web-Filtering"; Ensure = "Absent"},
+  @{Name = "Web-Basic-Auth"; Ensure = "Absent"},
+  @{Name = "Web-Windows-Auth"; Ensure = "Absent"},
+  @{Name = "Web-App-Dev"; Ensure = "Absent"},
+  @{Name = "Web-Net-Ext45"; Ensure = "Absent"},
+  @{Name = "Web-Asp-Net45"; Ensure = "Absent"},
+  @{Name = "Web-ISAPI-Ext"; Ensure = "Absent"},
+  @{Name = "Web-ISAPI-Filter"; Ensure = "Absent"},
+  @{Name = "Web-Ftp-Server"; Ensure = "Absent"},
+  @{Name = "Web-Mgmt-Tools"; Ensure = "Absent"},
+  @{Name = "Web-Mgmt-Console"; Ensure = "Absent"}     
+ ) #end array
+
+ node localhost {
+  foreach ($feature in $features){
+   WindowsFeature ($feature.Name) {
+    Name = $feature.Name
+    Ensure = $feature.Ensure
+   } #end WindowsFeature
+  } #end foreach
+ } #end node
+} #end configuration
