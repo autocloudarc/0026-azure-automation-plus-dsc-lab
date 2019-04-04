4175d6ce83400f923f7086be70ead032f8e16ac2
autocloudarc <prestopa@microsoft.com>
2019-04-04 01:53:32 -0400

    Add browser authentication prompt.

--- Deploy-AzureResourceGroup.ps1
+++ Deploy-AzureResourceGroup.ps1
@@ -97,6 +97,9 @@ if (Get-InstalledModule -Name $azureNonPreferredModule -ErrorAction SilentlyCont
 # Connect to Azure
 Connect-AzAccount
 
+$connectToSubscription = read-host "Your browser authentication request may be running the background. Please resize this window to see your browser authentication request to log into your Azure subscription"
+pause
+
 # Allowable student numbers
 [int[]]$studentNumEnum = 0..16
 
