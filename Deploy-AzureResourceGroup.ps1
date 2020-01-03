5beb86ac28d316328b2dc928da42a5ca8d990745
autocloudarc <prestopa@microsoft.com>
2020-01-03 09:35:55 -0500

    Update DC and Web VM images to 2019.

--- Deploy-AzureResourceGroup.ps1
+++ Deploy-AzureResourceGroup.ps1
@@ -7,7 +7,7 @@
    	Creates an automation lab to practice Azure automation, DSC, PowerShell and PowerShell core.
 
     The MIT License (MIT)
-    Copyright (c) 2018 Preston K. Parsard
+    Copyright (c) 2020 Preston K. Parsard
 
     Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
     to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
