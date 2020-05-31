4f4ed2235f464a8232d7a49f8b01bba80dd15fdc
autocloudarc <prestopa@microsoft.com>
2020-05-31 14:38:48 -0400

    Update message for removing availability sets.

--- Deploy-AzureResourceGroup.ps1
+++ Deploy-AzureResourceGroup.ps1
@@ -396,10 +396,10 @@ else
     $avSets = Get-AzAvailabilitySet -ResourceGroupName $rg
     foreach ($avSet in $avSets)
     {
-        # Cleanup availability sets that aren't assined to any virtual machines
-        Write-Output "Removing availability sets that are not associated with any virtual machines."
         if ($avSet.VirtualMachinesReferences.Id.Count -eq 0)
         {
+            # Cleanup availability sets that aren't assined to any virtual machines
+            Write-Output "Removing availability set that is not associated with any virtual machines."
             Remove-AzAvailabilitySet -ResourceGroupName $rg -Name $avSet.Name -Force -Verbose
         } # end if
     } # end foreach
